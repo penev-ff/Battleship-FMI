c6e52346c765b6c418114beb6db63b5432e96d06
Ivan Penev <ipvdevs@gmail.com>
2021-01-07 12:30:47 +0200

    Select loaded boards feature - build now if selection is failed

--- preload/preBoardConfig.cpp
+++ preload/preBoardConfig.cpp
@@ -1,9 +1,12 @@
+#include "./preBoardConfig.h"
 #include "../board/board.h"
 #include <fstream>
 #include <iostream>
 #include <limits>
+#include <string>
+#include <vector>
 
-const unsigned MAX_LINE_SIZE = 15;
+std::vector<std::string> loadedEntries;
 
 void corruptedFileError() {
   std::cout << "Error: Your preBoardEntries file is corrupted." << std::endl;
@@ -11,7 +14,7 @@ void corruptedFileError() {
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
-bool printSavedBoards() {
+bool loadEntries() {
   std::cout << "These are your preload saved boards:" << std::endl;
 
   std::ifstream entries;
@@ -22,23 +25,60 @@ bool printSavedBoards() {
     return false;
   }
 
-  char boardEntrie[MAX_LINE_SIZE] = "";
-  while (entries.getline(boardEntrie, MAX_LINE_SIZE)) {
+  std::string boardEntrie;
+  while (getline(entries, boardEntrie)) {
     std::cin.clear();
+
     if (boardEntrie[0] != '\0') {
-      std::cout << boardEntrie << std::endl;
+      loadedEntries.push_back(boardEntrie);
     }
   }
   return true;
 }
 
 bool loadBoard(int board[BOARD_SIZE][BOARD_SIZE]) {
-  bool loadPreBoardEntries = printSavedBoards();
-  if (!loadPreBoardEntries) {
-    return false;
+
+  bool isLoadSuccessfull;
+
+  if (loadedEntries.size() <= 0) {
+    isLoadSuccessfull = loadEntries();
+    if (!isLoadSuccessfull) {
+      return false;
+    }
   }
 
+  for (unsigned i = 0; i < loadedEntries.size(); i++) {
+    std::string loadedBoard = loadedEntries[i];
+    std::cout << i << '.';
+    int prtIndex = 0;
+    while (loadedBoard[prtIndex] != '.') {
+      std::cout << loadedBoard[prtIndex];
+      ++prtIndex;
+    }
+    std::cout << std::endl;
+  }
+
+  char selectedIndex;
+  std::cout << "Select a board to load:\n"
+            << "-> ";
+  std::cin >> selectedIndex;
+  std::cin.sync();
+  if (selectedIndex < '0' || selectedIndex > (char)('0' + (loadedEntries.size() - 1))) {
+    std::cout << "Failed to load a board.\n";
+    std::cout << "Invalid selection\n";
+    std::cout << "Press ENTER to continue... " << std::flush;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    isLoadSuccessfull = false;
+  }
+
+  if (!isLoadSuccessfull)
+  {
+    return false;
+  }
+  
+
+  std::cout << "Board loaded successfully!" << std::endl;
   std::cout << "Press ENTER to continue... " << std::flush;
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  return true;
+  return isLoadSuccessfull;
 }
--- preload/preBoardConfig.h
+++ preload/preBoardConfig.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <vector>
 #include "../board/board.h"
 
+bool loadEntries();
 void corruptedFileError();
 bool loadBoard(int board[BOARD_SIZE][BOARD_SIZE]);
